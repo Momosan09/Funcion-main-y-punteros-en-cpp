76778459845f8cee92ea5899b6a2c078cba856ed
Momosan09 <80794773+Momosan09@users.noreply.github.com>
2022-06-07 17:32:42 -0300

    Add files via upload

---
+++ FuncionMain_Punteros.cpp
@@ -0,0 +1,24 @@
+/*FUNCIONES*/
+
+funcion main -> funcion principal de los programas en c++
+
+1) void main()  --> No recibe parametros ni retorna nada
+
+2) int main()   --> No recibe parametros y retorna un entero al sistema operativo
+       main()   --> Un codigo de error generalmente negativo o un cero si no hay errores
+
+3) void main(int asd, char * asd[]) --> recibe un vector con "asd" cadena de caracteres  y no retorna/devuelve nada
+
+4) int main(int asd, char  asd[])   --> recibbe un array con "asd" cadena de caracteres y retorna un codigo de error al sistema operativo
+
+/*PUNTEROS*/
+
+int *n; -->   Puntero a un entero
+
+int v[20]; -->   Declaro un vector de 20 lugares enteros
+
+int *v[20]; -->   Vector de 20 lugares enteros, de tipo puntero
+
+* -->   Para usar el puntero, indica que usaremos el valor en la posicion de memoria apuntada
+
+& -->   Obtener la direccion de memoria de una variable
